a861c2c0998d398d8a76bfe2db7b71395490bbd7
moneko <poi.nyaa@qq.com>
2023-09-21 01:01:45 +0800

    support neko-ui

--- config/index.ts
+++ config/index.ts
@@ -38,6 +38,7 @@ const conf: Partial<ConfigType> = {
             'react-router',
             'react-router-dom',
             'dayjs',
+            'neko-ui',
           ],
         },
       ],
--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,12 +1,14 @@
 import React, { type FC } from 'react';
 import dayjs from 'dayjs';
 import Card from 'demo_remote_lib/Card';
+import 'neko-ui';
 
 const Root: FC = () => {
   return (
     <>
       <p>Root Page</p>
       <Card title={`当前时间: ${dayjs().format('YYYY-MM-DD HH:mm:ss')}`} />
+      <n-cron />
     </>
   );
 };

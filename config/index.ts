8dde1af607414351625d36ac67618798d56f25f2
moneko97 <poi.nyaa@qq.com>
2024-06-17 11:44:49 +0800

    更新依赖

--- config/index.ts
+++ config/index.ts
@@ -17,30 +17,30 @@ const conf: Partial<ConfigType> = {
       transform: '${member}',
     },
   },
-  // moduleFederation: [
-  //   {
-  //     name: 'demo_micro_app_react',
-  //     // 接入 moduleFederation
-  //     remotes: [
-  //       {
-  //         // moduleFederation的名称
-  //         name: 'demo_module_federation',
-  //         // moduleFederation 的地址
-  //         host: 'https://monako97.github.io/demo-module-federation',
-  //         // 从 moduleFederation 载入的依赖
-  //         library: [
-  //           'react',
-  //           'react/jsx-runtime',
-  //           'react-dom',
-  //           'react-dom/client',
-  //           'react-router',
-  //           'react-router-dom',
-  //           'dayjs',
-  //         ],
-  //       },
-  //     ],
-  //   },
-  // ],
+  moduleFederation: [
+    {
+      name: 'demo_micro_app_react',
+      // 接入 moduleFederation
+      remotes: [
+        {
+          // moduleFederation的名称
+          name: 'demo_module_federation',
+          // moduleFederation 的地址
+          host: 'https://monako97.github.io/demo-module-federation',
+          // 从 moduleFederation 载入的依赖
+          library: [
+            'react',
+            'react/jsx-runtime',
+            'react-dom',
+            'react-dom/client',
+            'react-router',
+            'react-router-dom',
+            'dayjs',
+          ],
+        },
+      ],
+    },
+  ],
 };
 
 export default conf;
